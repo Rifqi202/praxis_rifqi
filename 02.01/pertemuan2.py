9045d118b06a823e3c270f96b1f30e9c3738cdcc
Rifqi202 <febriawanrifqi@gmail.com>
2021-10-04 23:44:20 -0800

    belajar tipe data pada pertemuan kedua

---
+++ 02.01/pertemuan2.py
@@ -0,0 +1,68 @@
+print("belajar type data python")
+
+#tipe data string
+a = "Selamat Pagi"
+print(a)
+print(type(a))
+#menggabungkan string
+Z = "Selamat Pagi"
+X = "Praxis Academy"
+print(Z,X)
+print(Z+X)
+
+#tipe data dict
+b = {"nama" : "Rifqi Febriawan", "alamat" : "Bondowoso", "umur" : 21, "gender" : "laki-laki"}
+print(b)
+print(type(b))
+
+#tipe data integer & float
+data1 = 16
+data2 = 6.5
+penjumlahan = data1+data2
+print(penjumlahan)
+pengurangan = data1-data2
+print(pengurangan)
+pembagian = data1/data2
+print(pembagian)
+perkalian = data1*data2
+print(perkalian)
+print(type(data1))
+print(type(data2))
+
+#tipe data list
+gunung = ["sumbing","sindoro","slamet","merbabu","merapi"]
+#menampilkan isi list dengan indeks 2
+#print(gunung)
+print(gunung[2])
+#menampilkan isi list dari indeks 2 sampai selesai
+print(gunung[2:])
+#menampilkan isi list dari indeks 0 sampai 3
+print(gunung[0:3])
+#menampilkan isi list dengan indeks
+print(gunung[0])
+print(gunung[1])
+print(gunung[2])
+print(gunung[3])
+print(gunung[4])
+print(type(gunung))
+
+#tipe data tuple
+hewan = ('jangkrik', 'ulat', 'burung', 'kuda', 1,2,3,4)
+print (hewan[1])
+print (hewan[-1])
+#melihat jumlah data
+print (len(hewan))
+print(type(hewan))
+
+#tipe data boolean
+f = 12
+g = 10
+print(12 < 10)
+print(12 > 10)
+print("A" == "a")
+
+
+
+
+
+
